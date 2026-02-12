993ae370b68b520039f8593f4b7121dec01a37f4
kinggabse <112257460+kinggabse@users.noreply.github.com>
2026-02-12 17:23:17 +0100

    Create button.py

---
+++ custom_components/button.py
@@ -0,0 +1,35 @@
+from homeassistant.helpers.update_coordinator import CoordinatorEntity
+from homeassistant.components.button import ButtonEntity
+from homeassistant.helpers.device_registry import DeviceInfo
+
+from .const import DOMAIN, ICONS
+
+
+async def async_setup_entry(hass, entry, async_add_entities):
+    coordinator = hass.data[DOMAIN][entry.entry_id]
+
+    entities = []
+    for key in coordinator.counts:
+        entities.append(CleaningResetButton(coordinator, key))
+
+    async_add_entities(entities)
+
+
+class CleaningResetButton(CoordinatorEntity, ButtonEntity):
+    def __init__(self, coordinator, key):
+        super().__init__(coordinator)
+        self.key = key
+        self._attr_unique_id = f"{coordinator.entry.entry_id}_{key}_reset"
+        self._attr_name = f"{coordinator.entry.title} {key} Reinigung durchgeführt"
+
+        self._attr_device_info = DeviceInfo(
+            identifiers={(DOMAIN, coordinator.entry.entry_id)},
+            name=coordinator.entry.title,
+            manufacturer="Custom",
+            model="Cleaning Monitor",
+        )
+
+        self._attr_icon = ICONS["reset"]
+
+    async def async_press(self):
+        await self.coordinator.async_reset(self.key)
